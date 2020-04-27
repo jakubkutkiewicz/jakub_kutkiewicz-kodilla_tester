50feb5fdfb01b4fec315e910ba5d0f0952b58e18
Jakub Kutkiewicz <jakubkutkiewicz@gmail.com>
2020-04-27 18:41:15 +0200

    zadanie

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String hello = "hello from first class!";
+        String myName = "Maciej";
+        int myAge = 22;
+        double height = 1.78;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+        System.out.println(myAge);
+        System.out.println(hello);
+    }
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        int carsNumber = 1;
+        double capacityEngine = 1998;
+        boolean fast = true;
+        String carName = "Honda";
+        String carModel = "Civic";
+        char carType = 'R';
+        System.out.println(carsNumber);
+        System.out.println(capacityEngine);
+        System.out.println(fast);
+        System.out.println(carName);
+        System.out.println(carModel);
+        System.out.println(carType);
+    }
+}
