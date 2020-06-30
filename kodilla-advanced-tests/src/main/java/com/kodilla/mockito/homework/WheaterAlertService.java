b8cafa7cd62a15265f2249e25173bf715605e600
Jakub Kutkiewicz <jakubkutkiewicz@gmail.com>
2020-06-30 09:45:11 +0200

    WheaterAlertService

---
+++ kodilla-advanced-tests/src/main/java/com/kodilla/mockito/homework/WheaterAlertService.java
@@ -0,0 +1,58 @@
+package com.kodilla.mockito.homework;
+
+import java.util.*;
+
+public class WheaterAlertService {
+
+
+    private Map<String, List<Person>> persons = new HashMap<>();
+
+    public void addSubscriber(String position, Person person) {
+        if (persons.containsKey(position)) {
+            persons.get(position).add(person);
+        } else {
+            List<Person> personList = new ArrayList<>();
+            personList.add(person);
+            persons.put(position, personList);
+
+        }
+    }
+
+    public void removeSubscriber(String postion, Person person) {
+        if (persons.containsKey(postion)) {
+            persons.get(postion).remove(person);
+        }
+    }
+
+    public void removeAllSubscribers(Person person) {
+        if (persons.containsKey(person)) {
+            persons.remove(person);
+
+
+        }
+    }
+
+    public void sendNotification(Notification notification, String location) {
+        if (persons.containsKey(location)) {
+            persons.get(location).forEach(person -> person.receive(notification));
+        }
+    }
+
+    public void sendNotificationToAll(Notification notification) {
+        persons.values()
+                .stream()
+                .flatMap(toAll -> toAll.stream())
+                .forEach(person -> person.receive(notification));
+
+    }
+
+
+    public void removePosition(String position) {
+        persons.remove(position).stream()
+                .forEach(entry -> persons.remove(position));
+
+    }
+
+
+}
+
---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/mockito/homework/WheaterAlertTestSuite.java
@@ -0,0 +1,109 @@
+package com.kodilla.mockito.homework;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class WheaterAlertTestSuite {
+
+    @Test
+    public void subscribedPersonShouldReceiveNotificationFromPosition() {
+        WheaterAlertService wheaterAlertService = new WheaterAlertService();
+        Person person = Mockito.mock(Person.class);
+        Position postion = Mockito.mock(Position.class);
+        Notification notification = Mockito.mock(Notification.class);
+
+        wheaterAlertService.addSubscriber("Warszawa", person);
+        wheaterAlertService.sendNotification(notification, "Warszawa");
+
+        Mockito.verify(person, Mockito.times(1)).receive(notification);
+    }
+
+    @Test
+    public void canRemoveNotificationFromPosition() {
+        WheaterAlertService wheaterAlertService = new WheaterAlertService();
+        Person person = Mockito.mock(Person.class);
+        Position postion = Mockito.mock(Position.class);
+        Notification notification = Mockito.mock(Notification.class);
+
+
+        wheaterAlertService.addSubscriber("Warszawa", person);
+
+        wheaterAlertService.removeSubscriber("Warszawa", person);
+        wheaterAlertService.sendNotification(notification, "Warszawa");
+
+        Mockito.verify(person, Mockito.never()).receive(notification);
+    }
+
+    @Test
+    public void removeNotificationFromAllPositions() {
+        WheaterAlertService wheaterAlertService = new WheaterAlertService();
+        Person person = Mockito.mock(Person.class);
+
+        Notification notification = Mockito.mock(Notification.class);
+
+        wheaterAlertService.sendNotification(notification, "Katowice");
+        wheaterAlertService.addSubscriber("Katowice", person);
+
+        wheaterAlertService.removeAllSubscribers(person);
+        Mockito.verify(person, Mockito.never()).receive(notification);
+    }
+
+    @Test
+    public void sentNotificationToPosition() {
+        WheaterAlertService wheaterAlertService = new WheaterAlertService();
+        Person person1 = Mockito.mock(Person.class);
+        Person person2 = Mockito.mock(Person.class);
+
+        Notification notification = Mockito.mock(Notification.class);
+        wheaterAlertService.sendNotification(notification, "Katowice");
+
+        wheaterAlertService.addSubscriber("Katowice", person1);
+        wheaterAlertService.addSubscriber("Katowice", person2);
+
+        wheaterAlertService.sendNotification(notification, "Katowice");
+        Mockito.verify(person1, Mockito.times(1)).receive(notification);
+        Mockito.verify(person2, Mockito.times(1)).receive(notification);
+
+
+    }
+
+    @Test
+    public void sentNotificationToAll() {
+
+        WheaterAlertService wheaterAlertService = new WheaterAlertService();
+        Person person1 = Mockito.mock(Person.class);
+        Person person2 = Mockito.mock(Person.class);
+        Person person3 = Mockito.mock(Person.class);
+
+        Notification notification = Mockito.mock(Notification.class);
+
+
+        wheaterAlertService.addSubscriber("Katowice", person1);
+        wheaterAlertService.addSubscriber("Warszawa", person2);
+        wheaterAlertService.addSubscriber("Katowice", person3);
+
+        wheaterAlertService.sendNotificationToAll(notification);
+
+
+    }
+
+    @Test
+    public void deletePosition() {
+        WheaterAlertService wheaterAlertService = new WheaterAlertService();
+        Person person1 = Mockito.mock(Person.class);
+        Person person2 = Mockito.mock(Person.class);
+        Person person3 = Mockito.mock(Person.class);
+
+        Notification notification = Mockito.mock(Notification.class);
+
+
+        wheaterAlertService.addSubscriber("Katowice", person1);
+        wheaterAlertService.addSubscriber("Warszawa", person2);
+        wheaterAlertService.addSubscriber("Katowice", person3);
+
+
+        wheaterAlertService.removePosition("Katowice");
+
+    }
+}
+
