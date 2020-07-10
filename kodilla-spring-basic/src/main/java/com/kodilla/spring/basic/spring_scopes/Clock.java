fe1df237c24b45d3b4149d72f534a9dbff0ea371
Jakub Kutkiewicz <jakubkutkiewicz@gmail.com>
2020-07-10 16:25:44 +0200

    9.5

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_scopes/Clock.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_scopes/Clock.java
@@ -3,17 +3,16 @@ package com.kodilla.spring.basic.spring_scopes;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Component
 @Scope("prototype")
 public class Clock {
 
+    LocalTime localTime = LocalTime.now();
 
-    public Clock() {
-        LocalTime localTime = LocalTime.now();
-        System.out.println(localTime);
+    public LocalTime Clock() {
+        return this.localTime;
 
 
     }
--- kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_scopes/ClockTest.java
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_scopes/ClockTest.java
@@ -13,18 +13,38 @@ class ClockTest {
 
 
     @Test
-    public void DateTest () {
+    public void timeTest () {
         ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic.spring_scopes");
         Clock firstTime = context.getBean(Clock.class);
         Clock secondTime = context.getBean(Clock.class);
         Clock thirdTime = context.getBean(Clock.class);
-        Assertions.assertNotEquals(firstTime,secondTime);
-        Assertions.assertNotEquals(secondTime,thirdTime);
-        Assertions.assertNotEquals(firstTime,thirdTime);
+        Assertions.assertEquals(firstTime.localTime, secondTime.localTime);
+        Assertions.assertEquals(secondTime.localTime, thirdTime.localTime);
+        Assertions.assertEquals(firstTime.localTime, thirdTime.localTime);
 
-        System.out.println(firstTime);
-        System.out.println(secondTime);
-        System.out.println(thirdTime);
-        //czas dla wszystkich beanow jest taki sam
+        System.out.println(firstTime.localTime);
+        System.out.println(secondTime.localTime);
+        System.out.println(thirdTime.localTime);
+
+
+    }
+        @Test
+    public void differentTimeTest() throws InterruptedException {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic.spring_scopes");
+        Clock firstTime = context.getBean(Clock.class);
+        Thread.sleep(100);
+        Clock secondTime = context.getBean(Clock.class);
+        Thread.sleep(200);
+        Clock thirdTime = context.getBean(Clock.class);
+        Assertions.assertNotEquals(firstTime.localTime,secondTime.localTime);
+        Assertions.assertNotEquals(secondTime.localTime,thirdTime.localTime);
+        Assertions.assertNotEquals(firstTime.localTime,thirdTime.localTime);
+
+
+        System.out.println(firstTime.localTime);
+        System.out.println(secondTime.localTime);
+        System.out.println(thirdTime.localTime);
+
+
     }
 }
